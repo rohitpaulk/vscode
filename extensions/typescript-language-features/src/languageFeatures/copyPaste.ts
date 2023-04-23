437a4f5cb8bf3a66830634a1c7915a1ca57282ce
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-04-23 14:35:56 +0100

    basic copy

--- extensions/typescript-language-features/src/languageFeatures/copyPaste.ts
+++ extensions/typescript-language-features/src/languageFeatures/copyPaste.ts
@@ -7,9 +7,14 @@ import * as vscode from 'vscode';
 import { DocumentSelector } from '../configuration/documentSelector';
 
 class PasteEditProvider implements vscode.DocumentPasteEditProvider {
+	static readonly importsMimeType = 'application/vnd.code.typescript-language-features.copyPaste.imports-in-file';
+	static readonly sourceFilePathMimeType = 'application/vnd.code.typescript-language-features.copyPaste.source-file-path';
+
 	async prepareDocumentPaste(document: vscode.TextDocument, ranges: readonly vscode.Range[], dataTransfer: vscode.DataTransfer, token: vscode.CancellationToken): Promise<void> {
 		console.log('prepareDocumentPaste', document, ranges, dataTransfer, token);
-		return undefined;
+
+		dataTransfer.set(PasteEditProvider.importsMimeType, new vscode.DataTransferItem('import * as vscode from "vscode";'));
+		dataTransfer.set(PasteEditProvider.sourceFilePathMimeType, new vscode.DataTransferItem(document.uri.toString()));
 	}
 
 	async provideDocumentPasteEdits(
@@ -19,7 +24,15 @@ class PasteEditProvider implements vscode.DocumentPasteEditProvider {
 		token: vscode.CancellationToken,
 	): Promise<vscode.DocumentPasteEdit | undefined> {
 		console.log('provideDocumentPasteEdits', document, ranges, dataTransfer, token);
-		return undefined;
+		const imports = await dataTransfer.get(PasteEditProvider.importsMimeType)?.asString();
+		const sourceFilePath = await dataTransfer.get(PasteEditProvider.sourceFilePathMimeType)?.asString();
+
+		if (!imports || !sourceFilePath) {
+			return undefined; // Wasn't copied from a TS file that has our extension loaded
+		}
+
+		const text = await dataTransfer.get('text/plain')?.asString();
+		return new vscode.DocumentPasteEdit(`// override paste from ${sourceFilePath}\n\n${text}`);
 	}
 }
 
@@ -27,7 +40,7 @@ export function register(selector: DocumentSelector) {
 	console.log('registering paste provider');
 	return vscode.languages.registerDocumentPasteEditProvider(selector.syntax, new PasteEditProvider(), {
 		pasteMimeTypes: [
-			'*/*' // Try all for now
+			PasteEditProvider.importsMimeType,
 		]
 	});
 }
